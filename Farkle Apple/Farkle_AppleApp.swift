fc37ec50ad759b8cd58e75d8398f904bc9272b7a
Ethan Yan Xu <ethanxucoder@gmail.com>
2026-04-04 10:32:24 -0400

    Initial Commit

---
+++ Farkle Apple/Farkle_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Farkle_AppleApp.swift
+//  Farkle Apple
+//
+//  Created by Yan Xu on 2026-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Farkle_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
